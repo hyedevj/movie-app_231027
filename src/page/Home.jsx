fc19b9be76779927e968d0b202e2f27c17e22aea
hyedevj <hyedevj@gmail.com>
2023-11-07 16:08:59 +0900

    홈 화면 구현 1차

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,35 @@
-import Header from "../components/Header";
+import styled from "styled-components";
+import { data } from "../data/test";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Home() {
 	return (
-		<>
-      <Header />
-		</>
-	)
+		<Container>
+			<MoviePoster>
+				<Carousel
+					showThumbs
+					autoPlay
+					infiniteLoop
+					showStatus={false}
+					transitionTime={3}
+				>
+					{data.results.map((movie) => (
+						<div key={movie.id}>
+							<img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="" />
+						</div>
+						
+					))}
+				</Carousel>
+			</MoviePoster>
+		</Container>
+	);
 }
+
+const Container = styled.div``
+const MoviePoster = styled.div`
+	display: flex;
+	align-items: center;
+`
 
 export default Home
