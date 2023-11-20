9df2e58264c8a1afe0289829ad0ce66c7849302e
hyedevj <hyedevj@gmail.com>
2023-11-20 17:07:26 +0900

    검색기능 개발

--- src/page/MovieList.jsx
+++ src/page/MovieList.jsx
@@ -3,9 +3,16 @@ import { config } from "../data/constant";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import Card from "../components/Card";
+import SearchBox from "../components/SearchBox";
 
 function MovieList() {
   const [movies, setMovies] = useState([])
+
+  // 검색기능
+  const [keyword, setKeyword] = useState("")
+  const onChangeKeyword = (e) => {
+		setKeyword(e.target.value);
+	};
 
   const params = useParams()
 
@@ -14,14 +21,20 @@ function MovieList() {
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results)
+        setKeyword("")  // 페이지 넘어갈 때 검색창에 쓴 글자 지우기
       })
   }, [params.type])  // params.type이 바뀔 때 실행
 
   return (
     <Container>
+      <SearchBox keyword={keyword} onChangeKeyword={onChangeKeyword}></SearchBox>
       <Title></Title>
       <Group>
         {movies
+          .filter(
+            (movie) => movie.original_title.toLowerCase().includes(keyword.toLowerCase()) ||
+              movie.title.toLowerCase().includes(keyword.toLowerCase())
+          )
           .map((movie) =>
             <Card key={movie.id} movie={movie}></Card>
           )
